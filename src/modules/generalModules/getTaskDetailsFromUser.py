e4e9ac8aa06a6b2a8f7ce605442565886b179af4
Stannic <ayyan20218837@gmail.com>
2024-05-23 23:32:38 +0500

    round corners

--- src/modules/generalModules/getTaskDetailsFromUser.py
+++ src/modules/generalModules/getTaskDetailsFromUser.py
@@ -5,12 +5,20 @@ from PySide6.QtWidgets import (
     QDialog,
     QVBoxLayout,
     QHBoxLayout,
-    QDialogButtonBox
+    QDialogButtonBox,
+    QRadioButton,
+    QButtonGroup,
+    QFrame,
 )
+
 
 from PySide6.QtCore import (
     Qt
 )
+
+from modules.defaults.app_settings import CONFIG
+
+from modules.generalModules.taskData import TaskData
 
 
 class GetTaskDetailsDialog(QDialog):
@@ -18,19 +26,51 @@ class GetTaskDetailsDialog(QDialog):
         super().__init__()
 
         self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
 
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
+        self.window_frame = QFrame(parent=self)
+        self.window_frame.setObjectName("window_frame")
+        self.window_frame_layout = QVBoxLayout()
+        self.window_frame.setLayout(self.window_frame_layout)
+        self.layout.addWidget(self.window_frame)
+
         self.setUpTaskNameInputFeild()
+        self.setUpPriorityRadioButtons()
         self.setUpButtons()
         self.addWidgetsToMainLayout()
+        self.styleSheetSet()
 
     def setUpTaskNameInputFeild(self) -> None:
-        self.input_feild_task_name = QPlainTextEdit()
-        self.input_feild_task_name.setPlaceholderText("Task Name")
-        self.input_feild_task_name.setLineWrapMode(
+        self.task_name_plain_text = QPlainTextEdit()
+        self.task_name_plain_text.setPlaceholderText("Task Name")
+        self.task_name_plain_text.setLineWrapMode(
             QPlainTextEdit.LineWrapMode.WidgetWidth)
+
+    def setUpPriorityRadioButtons(self) -> None:
+        self.priority_button_container = QWidget()
+        self.priority_button_container_layout = QHBoxLayout()
+        self.priority_button_container.setLayout(
+            self.priority_button_container_layout)
+
+        self.priority_button_group = QButtonGroup()
+
+        self.high_priority_button = QRadioButton("High Priority")
+        self.mid_priority_button = QRadioButton("Middle Priority")
+        self.low_prioirty_button = QRadioButton("Low Priority")
+
+        self.priority_button_group.addButton(self.high_priority_button)
+        self.priority_button_group.addButton(self.mid_priority_button)
+        self.priority_button_group.addButton(self.low_prioirty_button)
+
+        self.priority_button_container_layout.addWidget(
+            self.high_priority_button)
+        self.priority_button_container_layout.addWidget(
+            self.mid_priority_button)
+        self.priority_button_container_layout.addWidget(
+            self.low_prioirty_button)
 
     def setUpButtons(self) -> None:
         self.buttons = QDialogButtonBox.StandardButton.Cancel | QDialogButtonBox.StandardButton.Save
@@ -41,15 +81,32 @@ class GetTaskDetailsDialog(QDialog):
         self.button_box.rejected.connect(self.reject)
 
     def addWidgetsToMainLayout(self) -> None:
-        self.layout.addWidget(self.input_feild_task_name)
-        self.layout.addWidget(self.button_box)
+        self.window_frame_layout.addWidget(self.task_name_plain_text)
+        self.window_frame_layout.addWidget(self.priority_button_container)
+        self.window_frame_layout.addWidget(self.button_box)
 
     def getTaskData(self) -> str:
-        return self.input_feild_task_name.toPlainText().strip()
+        data = TaskData(self.task_name_plain_text.toPlainText(), 1, 0, 0)
+        return data
 
     def on_accept(self):
-        if self.input_feild_task_name.toPlainText().strip():
+        if self.task_name_plain_text.toPlainText().strip():
             self.accept()
             return True
         else:
             self.reject()
+
+    def styleSheetSet(self) -> None:
+        self.setStyleSheet(f"""
+            QDialog {{
+                background-color : {CONFIG.global_background_color};
+                border : {CONFIG.global_border_thickness}px {CONFIG.global_border_type} {CONFIG.global_border_color};
+                border-radius : {CONFIG.dialogbox_border_roundness}px;
+                color : {CONFIG.global_font_color};
+            }}
+
+            #window_frame {{
+                border : {CONFIG.global_border_thickness}px {CONFIG.global_border_type} {CONFIG.global_border_color};
+                border-radius : {CONFIG.dialogbox_border_roundness}px;
+            }}
+        """)
